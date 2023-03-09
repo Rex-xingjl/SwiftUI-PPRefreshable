1860d9f08eb90f6cc3cda7b5d71f6f7fc8c04d76
Rex <xingjialiang@xunlei.com>
2023-03-09 11:35:10 +0800

    [fix] - Async/await 和 Dispatch框架存在异常

--- PPListDemo/PPListDemo/PPList/PPRefreshableScrollView.swift
+++ PPListDemo/PPListDemo/PPList/PPRefreshableScrollView.swift
@@ -15,7 +15,7 @@ public typealias OnRefresh = (@escaping RefreshCompleted) -> Void
 public let defaultRefreshThreshold: CGFloat = 89
 
 public enum RefreshState {
-    case waiting, primed, loading
+    case waiting, primed, loading, finishing
 }
 
 public typealias PPRefreshProgressBuilder<Progress: View> = (RefreshState) -> Progress
@@ -48,7 +48,7 @@ public struct PPRefreshableScrollView<Progress, Content>: View where Progress: V
         PPOffsetableScrollView(axes: .vertical, showsIndicator: showsIndicators) { proxy in
             /// 在某些机型上 将Y直接设置给offset会造成SwiftUI循环刷新 这里尝试了很多次 确定为现在的逻辑
             let Y = proxy.y
-            if state == .loading { // If we're already loading, ignore everything
+            if state == .loading || state == .finishing { // If we're already loading, ignore everything
                 offset = Y
             } else {
                 if Y > threshold && state == .waiting {
@@ -58,7 +58,19 @@ public struct PPRefreshableScrollView<Progress, Content>: View where Progress: V
                     offset = Y
                     state = .loading
                     onRefresh {
-                        withAnimation { self.state = .waiting }
+                        Task {
+                            // Delay of 0.35 seconds (1 second = 1_000_000_000 nanoseconds)
+                            try? await Task.sleep(nanoseconds: 350_000_000)
+                            await MainActor.run { self.state = .finishing }
+                            // Delay of 1 seconds
+                            try? await Task.sleep(nanoseconds: 1_000_000_000)
+                            await MainActor.run { withAnimation { self.state = .waiting } }
+                        }
+                    }
+                } else {
+                    // iOS14会出现异常: Bound preference PPOffsetPreferenceKey tried to update multiple times per frame.
+                    if #available(iOS 15, *) {
+                        offset = Y
                     }
                 }
             }
@@ -69,11 +81,11 @@ public struct PPRefreshableScrollView<Progress, Content>: View where Progress: V
                         .foregroundColor(loadingViewBackgroundColor)
                         .frame(height: threshold)
                     progress(state)
-                }.offset(y: (state == .loading) ? -max(0, offset) : -threshold)
+                }.offset(y: (state == .loading || state == .finishing) ? -max(0, offset) : -threshold)
                     
                 content()
                     .alignmentGuide(.top) { _ in
-                        (state == .loading) ? -threshold + max(0, offset) : 0
+                        (state == .loading || state == .finishing) ? -threshold + max(0, offset) : 0
                     }
             }
         }
