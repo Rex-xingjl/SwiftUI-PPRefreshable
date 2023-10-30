b058de67b79f88e4b0f929662631a474314c4ba3
Rex <xingjialiang@xunlei.com>
2023-10-30 18:14:57 +0800

    [fix] - Now, Animation is smoothly!

--- PPListDemo/PPListDemo/PPList/PPRefreshable.swift
+++ PPListDemo/PPListDemo/PPList/PPRefreshable.swift
@@ -8,6 +8,8 @@
 import Foundation
 import SwiftUI
 
+public typealias RefreshCompleted = () -> Void
+
 @available(iOS 14.0, *)
 public extension View {
     
@@ -19,18 +21,22 @@ public extension View {
         modifier(PPRefreshable(isRefreshing: isRefreshing, showsIndicators: showsIndicators, progress: { state in
             PPRefreshHeader(play: .constant(state.animating))
                 .frame(height: defaultRefreshThreshold)
-        }, refreshAction: refreshAction))
+        }, refreshAction: {
+            await withCheckedContinuation { continuation in
+                refreshAction { continuation.resume() }
+            }
+        }))
     }
     
     /// [async方式]对视图增加头部刷新
     /// - Parameter isRefreshing: 绑定当前刷新状态值
     /// - Parameter showsIndicators: 是否展示ScrollView的指示器
     /// - Parameter refreshAction: 刷新方法 结束后回调block
-    @ViewBuilder func pp_refreshable(_ isRefreshing: Binding<Bool>, showsIndicators: Bool = true, asyncRefreshAction: @escaping @Sendable () async -> Void) -> some View {
+    @ViewBuilder func pp_refreshable(_ isRefreshing: Binding<Bool>, showsIndicators: Bool = true, refreshAction: @escaping @Sendable () async -> Void) -> some View {
         modifier(PPRefreshable(isRefreshing: isRefreshing, showsIndicators: showsIndicators, progress: { state in
             PPRefreshHeader(play: .constant(state.animating))
                 .frame(height: defaultRefreshThreshold)
-        }, asyncRefreshAction: asyncRefreshAction))
+        }, refreshAction: refreshAction))
     }
 }
 
@@ -41,28 +47,18 @@ public struct PPRefreshable<Progress>: ViewModifier where Progress: View {
     @Binding private var isRefreshing: Bool
     private let showsIndicators: Bool
     private let progress: PPRefreshProgressBuilder<Progress>
+    private var refreshAction: (@Sendable () async -> Void)?
     
-    private var refreshAction: ((RefreshCompleted?) -> Void)?
-    private var asyncRefreshAction: (@Sendable () async -> Void)?
-    
-    public init(isRefreshing: Binding<Bool>, showsIndicators: Bool, @ViewBuilder progress: @escaping PPRefreshProgressBuilder<Progress>, refreshAction: @escaping (RefreshCompleted?) -> Void) {
+    public init(isRefreshing: Binding<Bool>, showsIndicators: Bool = true, @ViewBuilder progress: @escaping PPRefreshProgressBuilder<Progress>, refreshAction: @escaping @Sendable () async -> Void) {
         _isRefreshing = isRefreshing
         self.showsIndicators = showsIndicators
         self.progress = progress
         self.refreshAction = refreshAction
     }
-    
-    public init(isRefreshing: Binding<Bool>, showsIndicators: Bool = true, @ViewBuilder progress: @escaping PPRefreshProgressBuilder<Progress>, asyncRefreshAction: @escaping @Sendable () async -> Void) {
-        _isRefreshing = isRefreshing
-        self.showsIndicators = showsIndicators
-        self.progress = progress
-        self.asyncRefreshAction = asyncRefreshAction
-    }
 
     public func body(content: Content) -> some View {
-        PPRefreshableScrollView($isRefreshing, showsIndicators: showsIndicators, loadingViewBackgroundColor: .clear, threshold: defaultRefreshThreshold) { completion in
-            guard !isRefreshing else { completion(); return }
-            refreshHandler(completion)
+        PPRefreshableScrollView($isRefreshing, showsIndicators: showsIndicators, loadingViewBackgroundColor: .clear, threshold: defaultRefreshThreshold) {
+            await refreshAction?()
         } progress: { state in
             progress(state)
         } content: {
@@ -70,18 +66,4 @@ public struct PPRefreshable<Progress>: ViewModifier where Progress: View {
         }
     }
     
-    private func refreshHandler(_ completion: RefreshCompleted?) {
-        feedback.impactOccurred()
-        
-        if let refreshAction = refreshAction {
-            refreshAction {
-                Task { @MainActor in completion?() }
-            }
-        } else if let asyncRefreshAction = asyncRefreshAction {
-            Task {
-                await asyncRefreshAction()
-                await MainActor.run { completion?() }
-            }
-        }
-    }
 }
--- PPListDemo/PPListDemo/PPList/PPRefreshableScrollView.swift
+++ PPListDemo/PPListDemo/PPList/PPRefreshableScrollView.swift
@@ -8,27 +8,20 @@
 import Foundation
 import SwiftUI
 
-public typealias RefreshCompleted = () -> Void
-public typealias OnRefresh = (@escaping RefreshCompleted) -> Void
-
-fileprivate let PPOffsetSpaceName: String = "OffsetSpaceName"
-public struct PPOffsetPreferenceKey: PreferenceKey {
-    public static var defaultValue: CGPoint = .zero
-    public static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) { }
-}
-
-public let defaultRefreshThreshold: CGFloat = 89
+public typealias PPRefreshProgressBuilder<Progress: View> = (RefreshState) -> Progress
 
 public enum RefreshState {
     case waiting, primed, loading, finishing
     
-    var showRefreshView: Bool {
+    /// 保持住刷新状态视图的显示
+    var holdRefreshView: Bool {
         switch self {
         case .loading, .finishing: return true
         default: return false
         }
     }
     
+    /// 动画状态
     var animating: Bool {
         switch self {
         case .primed, .loading: return true
@@ -37,66 +30,90 @@ public enum RefreshState {
     }
 }
 
-public typealias PPRefreshProgressBuilder<Progress: View> = (RefreshState) -> Progress
+fileprivate let PPOffsetSpaceName: String = "OffsetSpaceName"
+public struct PPOffsetPreferenceKey: PreferenceKey {
+    public static var defaultValue: CGPoint = .zero
+    public static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) { }
+}
+
+/// 临界值
+public let defaultRefreshThreshold: CGFloat = 89
 
 public struct PPRefreshableScrollView<Progress, Content>: View where Progress: View, Content: View {
     let showsIndicators: Bool
     let loadingViewBackgroundColor: Color
     let threshold: CGFloat
-    let onRefresh: OnRefresh
     let progress: PPRefreshProgressBuilder<Progress>
     let content: () -> Content
+    
+    let refreshAction: @Sendable () async -> Void
+    
     @Binding private var isRefreshing: Bool
+    @State private var isRefreshEnding: Bool = false
     @State private var offset: CGFloat = 0
     @State private var state: RefreshState = .waiting // the current state
-
+    
     public init(_ isRefreshing: Binding<Bool>,
                 showsIndicators: Bool = true,
                 loadingViewBackgroundColor: Color = .clear,
                 threshold: CGFloat = defaultRefreshThreshold,
-                onRefresh: @escaping OnRefresh,
+                refreshAction: @escaping @Sendable () async -> Void,
                 @ViewBuilder progress: @escaping PPRefreshProgressBuilder<Progress>,
                 @ViewBuilder content: @escaping () -> Content) {
         _isRefreshing = isRefreshing
         self.showsIndicators = showsIndicators
         self.loadingViewBackgroundColor = loadingViewBackgroundColor
         self.threshold = threshold
-        self.onRefresh = onRefresh
+        self.refreshAction = refreshAction
         self.progress = progress
         self.content = content
     }
     
     public var body: some View {
         ScrollView(.vertical, showsIndicators: showsIndicators) {
-            VStack(spacing: 0) {
-                /// ScrollView无法修改spacing content与GeometryReader存在默认间距8 所以这里增加VStack
-                GeometryReader { proxy in
-                    Color.clear.preference(
-                        key: PPOffsetPreferenceKey.self,
-                        value: proxy.frame(in: .named(PPOffsetSpaceName)).origin
-                    )
+            ZStack {
+                VStack(spacing: 0) {
+                    /// ScrollView无法修改spacing content与GeometryReader存在默认间距8 所以这里增加VStack
+                    GeometryReader { proxy in
+                        Color.clear.preference(
+                            key: PPOffsetPreferenceKey.self,
+                            value: proxy.frame(in: .named(PPOffsetSpaceName)).origin
+                        )
+                    }
+                    .frame(width: 0.1, height: 0.1)
+                    
+                    Spacer()
                 }
-                .frame(width: 0.1, height: 0.1)
                 
-                ZStack {
-                    Rectangle()
-                        .foregroundColor(loadingViewBackgroundColor)
-                        .frame(height: threshold)
-                    progress(state)
+                VStack(spacing: 0) {
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(loadingViewBackgroundColor)
+                            .frame(height: threshold)
+                        progress(state)
+                    }
+                    .padding(.top, state.holdRefreshView ? 0 : offset-threshold)
+                    
+                    content()
                 }
-                .padding(.top, state.showRefreshView ? -max(0, offset) : -threshold)
-               
-                content()
             }
         }
         .ignoresSafeArea(.keyboard) /// 忽视其他界面弹起键盘导致的底部异常高度问题
         .coordinateSpace(name: PPOffsetSpaceName)
         .onPreferenceChange(PPOffsetPreferenceKey.self) { offsetChange($0) }
-        .onChange(of: isRefreshing) { if $0 { refreshStart() } }
+        .onChange(of: state) { value in
+            Task { @MainActor in
+                if value == .loading { await refreshProcess() }
+            }
+        }
+        .onChange(of: isRefreshing) { value in
+            Task { @MainActor in
+                withAnimation(.smooth) { state = value ? .loading : .waiting }
+            }
+        }
     }
-   
+    
     private func offsetChange(_ off: CGPoint) {
-        /// 这里需要主线程回传 规避一些线程异常情况
         Task { @MainActor in
             /// 在某些机型上 将Y直接设置给offset会造成SwiftUI循环刷新 这里尝试了很多次 确定为现在的逻辑
             let Y = off.y
@@ -113,26 +130,18 @@ public struct PPRefreshableScrollView<Progress, Content>: View where Progress: V
         }
     }
     
-    private func refreshStart() {
-        Task { @MainActor in
-            withAnimation { state = .loading }
-            onRefresh {
-                refreshEnd { isRefreshing = false }
-            }
-        }
+    @MainActor private func refreshProcess() async {
+        await refreshAction()
+        await refreshEnd()
     }
     
-    /// 结束动画
-    private func refreshEnd(_ back: (() -> Void)?) {
-        Task { @MainActor in
-            /// Delay of 0.35 seconds (1 second = 1_000_000_000 nanoseconds)
-            try? await Task.sleep(nanoseconds: 350_000_000)
-            self.state = .finishing
-            /// Delay of 1 seconds
-            try? await Task.sleep(nanoseconds: 1_000_000_000)
-            withAnimation { self.state = .waiting }
-            
-            back?()
-        }
+    @MainActor private func refreshEnd() async {
+        /// Delay of 0.35 seconds (1 second = 1_000_000_000 nanoseconds)
+        try? await Task.sleep(nanoseconds: 350_000_000)
+        self.state = .finishing
+        /// Delay of 1 seconds
+        try? await Task.sleep(nanoseconds: 1_000_000_000)
+        isRefreshing = false
     }
+    
 }
